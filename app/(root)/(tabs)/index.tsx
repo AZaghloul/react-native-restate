6e2c07ccd5462e4cdadea0b652eb705e82d2fb42
zaghloul <ahmd.zghlul@gmail.com>
2025-01-27 00:33:43 +0200

    add the project setup with the signin screen

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,23 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 16,
+      }}
+    >
+      <Text className="font-bold m-4 font-rubik text-3xl">
+        Welcome to ReState
+      </Text>
+      <Link href="/sign-in">Sign in</Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/profile">Profile</Link>
+      <Link href="/properties/123">Property</Link>
+    </View>
+  );
+}
